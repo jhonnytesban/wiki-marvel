a4e4960c12d1782c47c3e0a729b45c8a4a808e03
Jonathan <jonathanestebangalan@gmail.com>
2021-07-16 10:05:13 +0200

    n

--- src/pages/LoginPages.jsx
+++ src/pages/LoginPages.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MenuHamburguer from "../components/MenuHamburguer";
 import Slider from "../components/Slider";
+import HeroImage from '../assets/heroe-marvel2.jpg';
 
 import "../styles/loginPages.css";
 
@@ -13,6 +14,15 @@ const LoginPages = () => {
       </header>
       <main>
         <Slider />
+        {/* <hr /> */}
+        <section className='section-infoLogin'>
+          <div className="container-img-hero">
+            <img src={HeroImage} alt="" className="hero" />
+          </div>
+          <div className="infoLogin-content">
+            <p className="section__text">Regístrate y conoce todo el contenido Marvel</p>
+          </div>
+        </section>
       </main>
     </>
   );
